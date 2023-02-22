6098f3f053740e62b52c2ad4e1f088c48c03be91
liushida <liushida@topjoy.com>
2023-02-22 14:41:44 +0800

    Optimize

--- Auto/AutoUpdate.py
+++ Auto/AutoUpdate.py
@@ -13,19 +13,20 @@ def getUnityPath():
     return r
 
 def updateSVN():
-    subprocess.check_call("svn update", shell=True)
+    subprocess.check_call("svn update --accept=theirs-conflict", shell=True)
 
 def updateGit():
-    subprocess.check_call("git pull", shell=True)
+    subprocess.check_call("git fetch --all & git reset --hard & git pull", shell=True)
 
 def openUnity():
-    subprocess.check_call(UNITY_PATH + ' -projectPath ' + " ./Unity", shell=True)
+    subprocess.check_call(UNITY_PATH + ' -quit -projectPath ' + "./Unity ", shell=True)
 
 if __name__ == "__main__":
     UNITY_PATH = getUnityPath()
     for line in open("project.txt"):
         print(line)
-        os.chdir(line)
+        path = line.replace("\n", "")
+        os.chdir(path)
         updateSVN()
         updateGit()
         openUnity()
